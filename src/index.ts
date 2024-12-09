0cfca803f04b0ee27f2c8f46b0c061f0397394a7
DaviGabriel1 <davig41441503@gmail.com>
2024-12-09 09:30:02 -1200

    feat:roteamento inicial

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/",(req,res)=>{
+    res.send("teste");
+});
+
+app.listen(port,()=>{
+    console.log(`servidor rodando na porta ${port}`);
+});
+
+console.log("teste");
