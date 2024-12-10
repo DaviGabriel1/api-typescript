d59f220c23e84b4cafbea24597a6b7c20c22a0ac
DaviGabriel1 <davig41441503@gmail.com>
2024-12-10 00:23:43 -1200

    feat:GetUsersRepository get users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,16 +1,14 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository{
     constructor(){}
     async getUsers(): Promise<User[]> {
-        return [
-            {
-            firstName: "Davi",
-            lastName: "Santos",
-            email: "davi@gmail.com",
-            password: "123",
-        },
-    ];
+        const users = await MongoClient.db.
+        collection<Omit<User,"id">>("users").
+        find({}).toArray(); //omite o id do user da interface
+
+        return users.map(({_id,...rest}) => ({...rest, id: _id.toHexString()}));
     }
 }
